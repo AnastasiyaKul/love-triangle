59c707cef65fd74544de0cf79f980932c4c0b765
AnastasiyaKul <43112477+AnastasiyaKul@users.noreply.github.com>
2019-02-20 23:21:52 +0300

    e

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var countTriangles = 0;
+  for(let i = 0; i < preferences.length; i++) {
+    if(preferences[i] === i + 1) {
+      continue;
+    }
+    let secLover = preferences[preferences[i] - 1];
+    if(preferences[secLover - 1] === i + 1) {
+      countTriangles++;
+      delete(preferences[i]);
+    }
+  }
+
+  return countTriangles;
 };
